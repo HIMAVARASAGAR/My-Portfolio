f22ae9092a07b4db074dd7cd7bad5e4822236854
Kaki Himavara Sagar <kakihimavarasagar@Kakis-MacBook-Air.local>
2025-12-07 22:03:33 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { ghPages } from "vite-plugin-gh-pages";
 
-// Change base to your repo name **later**
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: "/my-portfolio/"
+  plugins: [react()],
+  // MUST match your repo name exactly (case-sensitive)
+  base: "/My-Portfolio/",
 });
